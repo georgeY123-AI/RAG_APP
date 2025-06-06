d4dacf96452be795312906c431a445513af45dfc
georgeY123-AI <georgeyoussed123@gmail.com>
2025-06-07 00:02:23 +0300

    FileHandling

--- src/routes/data.py
+++ src/routes/data.py
@@ -4,9 +4,10 @@ from helpers.config import get_settings, Settings
 from controllers import DataController, ProjectController
 from models import ResponseSignal
 import aiofiles
+import logging
 import os
 
-# from controllers.DataController import DataController
+logger = logging.getLogger('uvicorn.error')
 
 
 data_router = APIRouter(
@@ -19,8 +20,9 @@ async def upload_data(project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
      
      
+    data_controller = DataController()
      
-    is_valid, result_signal = DataController().validate_uploaded_file(file=file)
+    is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
     
     if not is_valid:
         return JSONResponse(
@@ -32,14 +34,25 @@ async def upload_data(project_id: str, file: UploadFile,
     
     
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = os.path.join(
-        project_dir_path,
-        file.filename
-    )
+    file_path = data_controller.generate_unique_filename(
+        orig_file_name=file.filename,
+        project_id=project_id
+        )
     
-    async with aiofiles.open(file_path, 'wb') as f:
-         while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
+    try:
+        async with aiofiles.open(file_path, 'wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        
+        logger.error(f"Failed to upload file: {e}")
+        
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+                }
+        )
     
     
     return JSONResponse(
