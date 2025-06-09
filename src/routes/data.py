dea68ffdb66e83094dd59ee26e3ac03d428546bf
georgeY123-AI <georgeyoussed123@gmail.com>
2025-06-09 18:32:48 +0300

    Updates

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,8 +5,10 @@ from controllers import DataController, ProjectController, ProcessController
 from models import ResponseSignal
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
-from models.db_schemes import DataChunk
+from models.db_schemes import DataChunk, Asset
 from models.ChunkModel import ChunkModel
+from models.AssetModel import AssetModel
+from models.enums.AssetTypeEnum import AssetTypeEnum
 import aiofiles
 import logging
 import os
@@ -24,7 +26,7 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
                       app_settings: Settings = Depends(get_settings)):
      
      
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
         )
      
@@ -65,13 +67,27 @@ async def upload_data(request: Request, project_id: str, file: UploadFile,
                 "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
                 }
         )
+        
+    asset_model = await AssetModel.create_instance(
+        db_client=request.app.db_client
+    )
+    
+    
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type= AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+    
+    
+    asset_record = await asset_model.create_asset(asset=asset_resource)
     
     
     return JSONResponse(
             content={
                 "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                "file_id": file_id
-                # "project_id": str(project.id)
+                "file_id": str(asset_record.id),
                 }
         )
     
@@ -83,7 +99,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
     chunk_size = process_request.chunk_size
     do_reset = process_request.do_reset
 
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
             db_client=request.app.db_client
             )
         
@@ -123,7 +139,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
 
 
 
-    chunk_model = ChunkModel(
+    chunk_model = await ChunkModel.create_instance(
         db_client=request.app.db_client
     )
     
